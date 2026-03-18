65481e316b22ef3822610196b7c8cb8a1b478fa0
sergiosboi <vitilus123@gmail.com>
2026-03-18 13:45:19 -0300

    Add remote cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,3 +30,38 @@ include("jadx-plugins:jadx-xapk-input")
 include("jadx-plugins:jadx-aab-input")
 include("jadx-plugins:jadx-apkm-input")
 include("jadx-plugins:jadx-apks-input")
+
+buildCache {
+	local {
+		// Disable local buildcache to maximize use of BuildFetch remote cache.
+		isEnabled = false
+	}
+
+	remote<HttpBuildCache> {
+		// On CI it's easiest to provide Env Vars
+		// On local macOS it's easier to provide ~/.gradle/gradle.properties for consistency between Terminal & IDE
+		val remoteUrl: String? = "JADX_GRADLE_REMOTE_CACHE_URL"
+			.let { System.getenv(it) ?: providers.gradleProperty(it).orNull }
+
+		val user: String? = "JADX_GRADLE_REMOTE_CACHE_USER"
+			.let { System.getenv(it) ?: providers.gradleProperty(it).orNull }
+
+		val token: String? = "JADX_GRADLE_REMOTE_CACHE_TOKEN"
+			.let { System.getenv(it) ?: providers.gradleProperty(it).orNull }
+
+		if (remoteUrl != null && user != null && token != null) {
+			isEnabled = true
+
+			url = uri(remoteUrl.trim())
+
+			credentials {
+				username = user.trim()
+				password = token.trim()
+			}
+
+			isPush = true
+		} else {
+			isEnabled = false
+		}
+	}
+}
